f93358e3ac1165714b5026781c3ff380da6508fc
bilal1994 <37197539+bilal1994@users.noreply.github.com>
2019-01-11 12:47:21 -0800

    Update home.ts

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController, AlertController } from 'ionic-angular';
 import { AdminPage } from '../admin/admin';
 import { LoginPage } from '../login/login';
+//  import { DNS } from '@ionic-native/dns';
 
 @Component({
   selector: 'page-home',
@@ -12,8 +13,44 @@ export class HomePage {
   Password : any;
 
   constructor(public navCtrl: NavController, public alert: AlertController) {
+  //   var malert = alert.create({
 
+  //     subTitle:"حدث خطأ",
+  
+  //     message:"تحقق من اتصالك بلانترنت او اعد المحاولة",
+  
+  //      cssClass:"setdire",
+  
+  //      enableBackdropDismiss:false
+  
+      
+  
+  //    })
+  
+  
+  
+  
+  //   var host = "www.google.com";
+  
+  
+  //   this.dns.resolve(host).then(addr => {
+  
+  // // اكو نت كمل شغلك 
+  
+  //  },err => {
+  
+  // // ماكو نت نضهر ال alert 
+  
+  //           malert.present();
+  
+        
+  
+  //         })
+  
+  
   }
+
+  
 
   admin() {
     const prompt = this.alert.create({
